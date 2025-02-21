d47973ec44a4163527a530db489f28ac4c328416
Grégory Fulgueiras <fulgueiras.gregory@gmail.com>
2025-02-22 00:04:23 +0100

    feat(footer): implement footer with links and navigaiton and create legal pages

---
+++ src/app/templates/legal-pages/cookie-management/cookie-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cookie-management',
+  imports: [],
+  templateUrl: './cookie-management.component.html',
+  styleUrl: './cookie-management.component.css'
+})
+export class CookieManagementComponent {
+
+}
---
+++ src/app/templates/legal-pages/policy-confidantiality/policy-confidantiality.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-policy-confidantiality',
+  imports: [],
+  templateUrl: './policy-confidantiality.component.html',
+  styleUrl: './policy-confidantiality.component.css'
+})
+export class PolicyConfidantialityComponent {
+
+}
