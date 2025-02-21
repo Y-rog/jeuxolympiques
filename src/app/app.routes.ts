dfe4079ec86d1649a57bf2703911009bdc7c6545
Grégory Fulgueiras <fulgueiras.gregory@gmail.com>
2025-02-22 00:04:23 +0100

    feat(footer): implement footer with links and navigaiton and create legal pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,17 @@ import { CartPageComponent } from './templates/cart-page/cart-page.component';
 import { HomePageComponent } from './templates/home-page/home-page.component';
 import { LoginPageComponent } from './templates/login-page/login-page.component';
 import { OffersPageComponent } from './templates/offers-page/offers-page.component';
+import { PolicyConfidantialityComponent } from './templates/legal-pages/policy-confidantiality/policy-confidantiality.component';
+import { CookieManagementComponent } from './templates/legal-pages/cookie-management/cookie-management.component';
+import { CgvComponent } from './templates/legal-pages/cgv/cgv.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomePageComponent},
     {path: 'offers', component: OffersPageComponent},
     {path: 'cart', component: CartPageComponent}, 
-    {path: 'login', component: LoginPageComponent},   
+    {path: 'login', component: LoginPageComponent},
+    {path: 'policy-confidantiality', component: PolicyConfidantialityComponent},
+    {path: 'cookie-management', component: CookieManagementComponent},
+    {path: 'cgv', component: CgvComponent},
+    {path: '', component: HomePageComponent}  
 ];
