86cd806a9fc22a2bca9f81170dabd2ea1a20105c
Grégory Fulgueiras <fulgueiras.gregory@gmail.com>
2025-03-11 23:37:46 +0100

    fix(heroku): debug heroku deployment

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Indique à Express de servir les fichiers statiques
-app.use(express.static(__dirname + '/dist/jeuxolympiques'));
+app.use(express.static(__dirname + '/dist/jeuxolympiques/browser/'));
 
 // Redirige toutes les requêtes vers index.html
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/jeuxolympiques/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/jeuxolympiques/browser/index.html'));
 });
 
 // Le port que Heroku choisira automatiquement
