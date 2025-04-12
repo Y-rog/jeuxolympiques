a45ee7ebe70bdcff5b49f6ce56d2616572617220
Grégory Fulgueiras <fulgueiras.gregory@gmail.com>
2025-04-12 21:39:11 +0200

    fix(debug): deployment heroku

--- src/environments/envrionment.prod.ts
+++ src/environments/envrionment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
+   // Ce fichier remplace le fichier par défaut lors de la construction de l'application Angular en mode production.
     production: true,
     apiUrl: 'https://api-jeuxolympiques.gregoryfulgueiras.com/api-jeuxolympiques'
   };
